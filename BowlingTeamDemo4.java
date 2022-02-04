9c632f5f226d52208cfa5eb002acb570871fa804
romeroarantza <92955647+romeroarantza@users.noreply.github.com>
2022-02-03 21:07:33 -0500

    Pass an Array to a method

---
+++ BowlingTeamDemo4.java
@@ -0,0 +1,75 @@
+import java.util.*;
+//encapsulate the data entry to a method
+
+public class BowlingTeamDemo4
+{
+   public static void main(String[] args)
+   {
+      String name;
+      final int NUM_TEAMS = 4;
+      int x; //subscript for team member names
+      int y; //subscript for teams
+      final int NUM_TEAM_MEMBERS = 4;
+      Scanner input = new Scanner(System.in);
+      
+      BowlingTeam[] teams = new BowlingTeam[4];
+      getTeamData(teams);
+     
+            
+      //display details of the team 
+      for(y = 0; y < NUM_TEAMS; ++y)
+      {
+         System.out.println("\nMembers of team " 
+         + teams[y].getTeam() + ":");
+         for(x = 0; x < NUM_TEAM_MEMBERS; ++x)
+            System.out.print(teams[y].getMember(x) + " ");
+         System.out.println();
+       }
+       System.out.print("\n\nEnter a team name to see its roster >>");
+       name = input.nextLine();
+     
+       boolean isValidTeam = false;
+     
+        for(y = 0; y < teams.length && !isValidTeam; ++y)
+        {
+            isValidTeam = name.equals(teams[y].getTeam());
+            if(isValidTeam)
+            {
+               for(x = 0; x < NUM_TEAM_MEMBERS; ++x)
+                  System.out.print(teams[y].getMember(x) + " ");
+            }
+         }
+     //searching the array for a match 
+          System.out.println();
+   }
+   public static void getTeamData(BowlingTeam[] teams)
+   {
+      String name;
+      final int NUM_TEAMS = 4;
+      int x; //subscript for team member names
+      int y; //subscript for teams
+      final int NUM_TEAM_MEMBERS = 4;
+      Scanner input = new Scanner(System.in);
+      
+      //allocate memory for each array element of the team
+      for(y = 0; y < NUM_TEAMS; ++y)
+      {
+         teams[y] = new BowlingTeam();
+         
+         //prompt for the bowling team name
+         System.out.println("Enter team name >> ");
+         name = input.nextLine();
+         teams[y].setTeamName(name);
+         
+         //in a loop prompt the members' names
+         for(x = 0; x < NUM_TEAM_MEMBERS; ++x)
+         {
+            System.out.print("Enter team member's name >> ");
+            name = input.nextLine();
+            teams[y].setMember(x, name);
+         }//nested loop
+      }// end of team for loop
+      
+
+   }
+}
