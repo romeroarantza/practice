ccf805273b3bdd8bcd9bb859a31642d6087d189b
romeroarantza <92955647+romeroarantza@users.noreply.github.com>
2022-04-12 11:25:09 -0400

    Check pattern with Regex

---
+++ RegexPattern.java
@@ -0,0 +1,25 @@
+package HackerRank;
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class RegexPattern {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int testCases = Integer.parseInt(sc.nextLine());
+		while(testCases > 0){
+			String pattern = sc.nextLine();
+			try {
+				Pattern.compile(pattern);
+				System.out.println("Valid");
+			} catch (PatternSyntaxException e) {
+				System.out.println("Invalid");
+			}
+			
+			testCases--;
+		}
+		sc.close();
+	}
+
+}
